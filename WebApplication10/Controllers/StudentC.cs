9bd0f818cc96fec40bc715a6244cb925e2184e08
Gauri <gaurijindal.hrd@gmail.com>
2024-05-07 21:44:53 +0530

    Add project files.

---
+++ WebApplication10/Controllers/StudentC.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class StudentC : Controller
+    {
+        public int StudentId { get; private set; }
+        public string? Name { get; private set; }
+        public DateTime EnrollmentDate { get; private set; }
+
+        public IActionResult Index()
+        {
+
+           
+            var students = new List<Student>
+            {
+                new Student { StudentId = 1, Name = "John Smith", EnrollmentDate = DateTime.Now },
+                new Student { StudentId = 2, Name = "Joey Tribbiani", EnrollmentDate = DateTime.Now }
+            };
+
+            return View(students);
+        }
+    }
+}
---
+++ WebApplication10/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication10.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the student's name.")]
+        public string ? Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
